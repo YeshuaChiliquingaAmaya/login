8649230e0a58b15dc505b05b7c1ab66f6a16419c
YeshuaChiliquingaAmaya <yeshuachiliquinga@gmail.com>
2025-01-04 18:57:40 -0500

    Actualización para evitar la repetición del correo y el usuario

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -11,6 +11,32 @@
     $query = "INSERT INTO usuarios (nombre_completo, correo, usuario, contrasena) 
               VALUES ('$nombre_completo', '$correo', '$usuario', '$contrasena')";
 
+    //Verificar que el correo no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo = '$correo'");
+
+    if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este correo ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+    //Verificar que el usuario no se repita en la base de datos
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario = '$usuario'");
+
+    if(mysqli_num_rows($verificar_usuario) > 0){
+        echo '
+            <script>
+                alert("Este usuario ya está registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
     // Crear la conexión
     $ejecutar = mysqli_query($conexion, $query);
 
